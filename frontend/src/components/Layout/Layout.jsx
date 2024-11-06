3e512331013f0a59f1a0a4ca2ad70237dd463f72
Tdmaxer <49441022+Tdmaxer@users.noreply.github.com>
2024-11-06 08:24:00 +0530

    app,index js basic setups to navigate

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Routers from '../../router/Routers';
+
 function Layout() {
   return (
     <div>
+      <Header />
+      <Routers />
+      <Footer />
       
     </div>
   )
