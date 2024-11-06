14dfd278a8ab4af91bed82f490c1fec577a26ff7
Tdmaxer <49441022+Tdmaxer@users.noreply.github.com>
2024-11-06 08:04:32 +0530

    Frontend foler basic structure setuped

---
+++ frontend/src/pages/SerchResultList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SerchResultList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SerchResultList
+
---
+++ frontend/src/router/Routers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const Routers = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Routers
